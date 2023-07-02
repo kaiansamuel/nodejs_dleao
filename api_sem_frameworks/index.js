3daa18cdbfee2bca4812875c60013b2ac2a5cd70
Kaian Samuel <kaiansamuelster@gmail.com>
2023-07-02 12:28:48 -0300

    Criando a primeira resposta

--- api_sem_frameworks/index.js
+++ api_sem_frameworks/index.js
@@ -1,7 +1,8 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-
+  response.write('Minha primeira resposta')
+  response.end()
 })
 
 server.listen(3030, () => { console.log('Servidor rodando!') })
