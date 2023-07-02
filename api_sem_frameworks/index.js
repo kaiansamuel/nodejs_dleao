972cf985b6935fe43517a64e047fa9d6e8ab7777
Kaian Samuel <kaiansamuelster@gmail.com>
2023-07-02 14:20:12 -0300

    Minha primeira rota

--- api_sem_frameworks/index.js
+++ api_sem_frameworks/index.js
@@ -1,18 +1,38 @@
-const { application } = require('express')
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  const result = {
-    message: 'Minha primeira resposta!'
+  if(request.url === '/minha-primeira-rota'){
+    const result = {
+      message: 'Minha primeira rota'
+    }
+    response.statusCode = 200
+    response.setHeader('Content-type', 'application/json')
+    
+    return response.end(JSON.stringify(result))
   }
-  response.statusCode = 201
-  response.writeHead(201, {
-    'Content-type': 'application/json'
-  })
-  response.statusMessage = 'Usuário salvo com sucesso!'
+  if(request.url === '/minha-segunda-rota'){
+    const result = {
+      message: 'Minha segunda rota'
+    }
+    response.statusCode = 200
+    response.setHeader('Content-type', 'application/json')
+    
+    return response.end(JSON.stringify(result))
+  }
+
+  const result = {
+    message: 'Qualquer outra rota!'
+  }
+  response.statusCode = 200
+  response.setHeader('Content-type', 'application/json')
+  response.statusMessage = 'Usuário faltando informação'
 
   response.write(JSON.stringify(result))
   response.end()
-})
+  })
 
-server.listen(3030, () => { console.log('Servidor rodando!') })
+  server.listen(3030, () => console.log('Servidor rodando!'))
+  
+  
+
+  
